37c0599e1e0d537e60bb152291508cc1f882d597
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:47:48 +0530

    code Hotel  class

--- src/main/java/com/ijse/Hotel_Management_System/entity/Hotel.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/Hotel.java
@@ -1,4 +1,57 @@
 package com.ijse.Hotel_Management_System.entity;
 
-public class Hotel {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "hotels")
+public class Hotel extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 2000)
+    private String description;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String address;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    private Double starRating;
+
+    private String phone;
+
+    private String email;
+
+    @Builder.Default
+    private boolean active = true;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<HotelImage> images = new ArrayList<>();
+
+    @Builder.Default
+    @OneToMany(mappedBy = "hotel", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Room> rooms = new ArrayList<>();
 }
