3c7b51fd10bb4b1f10b919d39b45c417bc38a0c0
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:43:21 +0530

    code Base BookingService class

--- src/main/java/com/ijse/Hotel_Management_System/entity/BookingService.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/BookingService.java
@@ -1,4 +1,49 @@
 package com.ijse.Hotel_Management_System.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "booking_services")
 public class BookingService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Booking booking;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "extra_service_id", nullable = false)
+    private ExtraService extraService;
+
+    @NotNull
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @NotNull
+    @Column(nullable = false)
+    private BigDecimal priceAtBooking;
 }
+
+
+
+
+
+
+
+
+
+
+
