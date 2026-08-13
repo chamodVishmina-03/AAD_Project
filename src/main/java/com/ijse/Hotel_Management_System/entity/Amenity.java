91c16c6ce6bc6e601ab6b85192dda957858663d8
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:39:07 +0530

    code amenity class and add jwt dependancies

--- src/main/java/com/ijse/Hotel_Management_System/entity/Amenity.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/Amenity.java
@@ -1,4 +1,27 @@
 package com.ijse.Hotel_Management_System.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "amenities", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class Amenity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+
+    private String icon;
 }
