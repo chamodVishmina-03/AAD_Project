da70c027368dc73ad70458bd2b368677649daf2c
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:44:11 +0530

    code City  class

--- src/main/java/com/ijse/Hotel_Management_System/entity/City.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/City.java
@@ -1,4 +1,28 @@
 package com.ijse.Hotel_Management_System.entity;
 
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "cities")
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String country;
 }
