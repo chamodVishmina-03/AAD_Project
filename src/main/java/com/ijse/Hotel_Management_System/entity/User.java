2a24a2df7ac8edec99e3264df032470f505cdf71
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:57:32 +0530

    code User class

--- src/main/java/com/ijse/Hotel_Management_System/entity/User.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/User.java
@@ -1,4 +1,50 @@
 package com.ijse.Hotel_Management_System.entity;
 
-public class User {
+import com.ijse.HOTEL_MANAGEMENT_SYSTEM.enumeration.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String fullName;
+
+    @Email
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    private String phone;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<Role> roles = new HashSet<>();
 }
