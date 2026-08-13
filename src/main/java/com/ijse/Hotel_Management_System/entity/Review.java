e9f386166dadc03df762aa20529d1fcf7afde13c
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:52:39 +0530

    code Review  class

--- src/main/java/com/ijse/Hotel_Management_System/entity/Review.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/Review.java
@@ -1,4 +1,38 @@
 package com.ijse.Hotel_Management_System.entity;
 
-public class Review {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "reviews")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    @NotNull
+    @Min(1)
+    @Max(5)
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(length = 2000)
+    private String comment;
 }
