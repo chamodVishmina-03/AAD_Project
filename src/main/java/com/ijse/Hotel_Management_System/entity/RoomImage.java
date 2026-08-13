07498f62efa86bd8de0d2d6ef47067401986ab6d
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:53:59 +0530

    code RoomImage  class

--- src/main/java/com/ijse/Hotel_Management_System/entity/RoomImage.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/RoomImage.java
@@ -1,4 +1,28 @@
 package com.ijse.Hotel_Management_System.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "room_images")
 public class RoomImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @Column(nullable = false, length = 1000)
+    private String imageUrl;
+
+    private String caption;
 }
