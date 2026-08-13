1b69ee86258194b019c94b8779b604d656cfe18d
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 07:44:44 +0530

    in it project

---
+++ src/main/java/com/ijse/Hotel_Management_System/HotelManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ijse.Hotel_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HotelManagementSystemApplication.class, args);
+	}
+
+}
