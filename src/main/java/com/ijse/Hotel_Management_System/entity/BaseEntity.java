5d5b7670b96a2cd34970d7fe525ead91d5b666e7
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:41:20 +0530

    code Base Entity class

--- src/main/java/com/ijse/Hotel_Management_System/entity/BaseEntity.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/BaseEntity.java
@@ -1,4 +1,42 @@
 package com.ijse.Hotel_Management_System.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-public class BaseEntity {
+import java.time.LocalDateTime;
+
+
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
