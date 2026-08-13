bf6f7bf1834dc27651c1a7c8ede35285a5c0ae97
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-13 14:54:32 +0530

    code RoomType  class

--- src/main/java/com/ijse/Hotel_Management_System/entity/RoomType.java
+++ src/main/java/com/ijse/Hotel_Management_System/entity/RoomType.java
@@ -1,4 +1,27 @@
 package com.ijse.Hotel_Management_System.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "room_types", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class RoomType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name; // e.g. SINGLE, DOUBLE, DELUXE, SUITE
+
+    private String description;
+
+    private Integer maxOccupancy;
 }
