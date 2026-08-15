8519b6f657e4fb856295feca4d6d95bc6221b7b9
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:04:46 +0530

    add  AuthResponsedto code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/response/AuthResponse.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/response/AuthResponse.java
@@ -1,4 +1,19 @@
 package com.ijse.Hotel_Management_System.dto.response;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+import java.util.Set;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class AuthResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long userId;
+    private String fullName;
+    private String email;
+    private Set<String> roles;
 }
