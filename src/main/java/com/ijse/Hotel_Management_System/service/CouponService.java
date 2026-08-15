171e9d4d176ab91a928fda1295cdd2a437474c63
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:21:27 +0530

    update service file

--- src/main/java/com/ijse/Hotel_Management_System/service/CouponService.java
+++ src/main/java/com/ijse/Hotel_Management_System/service/CouponService.java
@@ -1,7 +1,7 @@
-package com.ijse.HOTEL_MANAGEMENT_SYSTEM.service;
+package com.ijse.Hotel_Management_System.service;
 
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.request.CouponRequest;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.entity.Coupon;
+import com.ijse.Hotel_Management_System.dto.request.CouponRequest;
+import com.ijse.Hotel_Management_System.entity.Coupon;
 
 import java.util.List;
 
