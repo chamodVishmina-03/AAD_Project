01a5ad17ed5dadf1164f8bb627f7029be15b130a
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:42:23 +0530

    add CoupenRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/CouponRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/CouponRequest.java
@@ -1,4 +1,16 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class CouponRequest {
-}
+import com.ijse.HOTEL_MANAGEMENT_SYSTEM.enumeration.DiscountType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CouponRequest(
+        @NotBlank String code,
+        @NotNull DiscountType discountType,
+        @NotNull BigDecimal discountValue,
+        BigDecimal minBookingAmount,
+        LocalDate expiryDate
+) {}
