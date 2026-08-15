d1311b339414c5e31d1ff21e157446de97d8d3cd
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 07:28:38 +0530

    add  userResponsedto code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/response/UserResponse.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/response/UserResponse.java
@@ -1,4 +1,20 @@
 package com.ijse.Hotel_Management_System.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String phone;
+    private boolean active;
+    private Set<String> roles;
 }
