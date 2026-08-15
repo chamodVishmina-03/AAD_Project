1fdf6bb96126fef74905386a3c5c255eef4b3ce3
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:34:49 +0530

    add PaymentRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/PaymentRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/PaymentRequest.java
@@ -1,4 +1,6 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class PaymentRequest {
-}
+import com.ijse.HOTEL_MANAGEMENT_SYSTEM.enumeration.PaymentMethod;
+import jakarta.validation.constraints.NotNull;
+
+public record PaymentRequest(@NotNull Long bookingId, @NotNull PaymentMethod method) {}
