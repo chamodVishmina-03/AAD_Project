f8be9cce7575cf99279fd080d1753b4e1c94273a
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:47:37 +0530

    add AmenityRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/AmenityRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/AmenityRequest.java
@@ -1,4 +1,5 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class AmenityRequest {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record AmenityRequest(@NotBlank String name, String description, String icon) {}
