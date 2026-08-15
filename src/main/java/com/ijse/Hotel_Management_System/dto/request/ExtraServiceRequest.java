844edd2d2086905332b18ca926e949b379eb7a49
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:41:32 +0530

    add ExtraServicesRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/ExtraServiceRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/ExtraServiceRequest.java
@@ -1,4 +1,13 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class ExtraServiceRequest {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ExtraServiceRequest(
+        @NotNull Long hotelId,
+        @NotBlank String name,
+        String description,
+        @NotNull BigDecimal price
+) {}
