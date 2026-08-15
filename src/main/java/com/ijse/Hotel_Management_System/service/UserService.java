78a493e1d5a2dfc746231365ecb56f8b93466f58
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:26:38 +0530

    update service file

--- src/main/java/com/ijse/Hotel_Management_System/service/UserService.java
+++ src/main/java/com/ijse/Hotel_Management_System/service/UserService.java
@@ -1,8 +1,8 @@
-package com.ijse.HOTEL_MANAGEMENT_SYSTEM.service;
+package com.ijse.Hotel_Management_System.service;
 
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.request.UpdateUserRequest;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.response.UserResponse;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.enumeration.Role;
+import com.ijse.Hotel_Management_System.dto.request.UpdateUserRequest;
+import com.ijse.Hotel_Management_System.dto.response.UserResponse;
+import com.ijse.Hotel_Management_System.enumeration.Role;
 
 import java.util.List;
 
