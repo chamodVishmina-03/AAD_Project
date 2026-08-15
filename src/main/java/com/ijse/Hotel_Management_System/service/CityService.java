b279b325475c9b3a4f2f8005ae7a567243e02ff7
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:21:11 +0530

    update service file

--- src/main/java/com/ijse/Hotel_Management_System/service/CityService.java
+++ src/main/java/com/ijse/Hotel_Management_System/service/CityService.java
@@ -1,7 +1,7 @@
-package com.ijse.HOTEL_MANAGEMENT_SYSTEM.service;
+package com.ijse.Hotel_Management_System.service;
 
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.request.CityRequest;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.entity.City;
+import com.ijse.Hotel_Management_System.dto.request.CityRequest;
+import com.ijse.Hotel_Management_System.entity.City;
 
 import java.util.List;
 
