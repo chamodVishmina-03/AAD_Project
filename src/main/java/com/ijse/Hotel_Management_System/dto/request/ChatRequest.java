0d0af751f1713c07a936d30f81178bd9a3defd0a
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:43:37 +0530

    add ChatRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/ChatRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/ChatRequest.java
@@ -1,4 +1,8 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class ChatRequest {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChatRequest(
+        @NotBlank @Size(max = 1000, message = "Keep messages under 1000 characters") String message
+) {}
