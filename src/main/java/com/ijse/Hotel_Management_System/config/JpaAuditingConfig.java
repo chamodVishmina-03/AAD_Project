f809548c6fb1e9f3e9f05ac8d8640a5e053498e2
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:58:58 +0530

    Update config files

--- src/main/java/com/ijse/Hotel_Management_System/config/JpaAuditingConfig.java
+++ src/main/java/com/ijse/Hotel_Management_System/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.ijse.HOTEL_MANAGEMENT_SYSTEM.config;
+package com.ijse.Hotel_Management_System.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
