e7596751f04664a59dee61fb5b030dcb944362ab
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 12:44:30 +0530

    finished  BookingStatus  code

--- src/main/java/com/ijse/Hotel_Management_System/enumeration/BookingStatus.java
+++ src/main/java/com/ijse/Hotel_Management_System/enumeration/BookingStatus.java
@@ -1,4 +1,11 @@
 package com.ijse.Hotel_Management_System.enumeration;
 
 public enum BookingStatus {
+    PENDING,
+    CONFIRMED,
+    CHECKED_IN,
+    CHECKED_OUT,
+    CANCELLED,
+    COMPLETED
 }
+
