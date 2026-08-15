d1e016561e0fe3bf720d60b5ac02e17c86152226
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:42:54 +0530

    add CoupenRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/CityRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/CityRequest.java
@@ -1,4 +1,5 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class CityRequest {
-}
+import jakarta.validation.constraints.NotBlank;
+
+public record CityRequest(@NotBlank String name, @NotBlank String country) {}
