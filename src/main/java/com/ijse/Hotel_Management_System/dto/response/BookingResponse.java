361c13145a124ff5cecd331427f05a355add6da7
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:03:21 +0530

    add  BookingResponsedto code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/response/BookingResponse.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/response/BookingResponse.java
@@ -1,4 +1,27 @@
 package com.ijse.Hotel_Management_System.dto.response;
+import com.ijse.HOTEL_MANAGEMENT_SYSTEM.enumeration.BookingStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class BookingResponse {
+    private Long id;
+    private Long userId;
+    private String guestName;
+    private Long roomId;
+    private String roomNumber;
+    private String hotelName;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private Integer numberOfGuests;
+    private BookingStatus status;
+    private BigDecimal totalAmount;
+    private List<String> extraServices;
 }
