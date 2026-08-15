20cba4a069b0d2044cf8614ac451b20cfbd1c2aa
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:00:39 +0530

    add  Hotel Responsedto code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/response/HotelResponse.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/response/HotelResponse.java
@@ -1,4 +1,24 @@
 package com.ijse.Hotel_Management_System.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class HotelResponse {
+    private Long id;
+    private String name;
+    private String description;
+    private String address;
+    private String cityName;
+    private String country;
+    private Double starRating;
+    private String phone;
+    private String email;
+    private boolean active;
+    private List<String> imageUrls;
 }
