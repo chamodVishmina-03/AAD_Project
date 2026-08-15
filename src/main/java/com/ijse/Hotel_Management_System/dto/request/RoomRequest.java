3a020857c2567eb3a6c7de5bdb5ea0db5d74752c
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:16:26 +0530

    add  RoomRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/RoomRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/RoomRequest.java
@@ -1,4 +1,17 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class RoomRequest {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public record RoomRequest(
+        @NotNull Long hotelId,
+        @NotNull Long roomTypeId,
+        @NotBlank String roomNumber,
+        Integer floorNo,
+        @NotNull @Positive BigDecimal pricePerNight,
+        Set<Long> amenityIds
+) {}
