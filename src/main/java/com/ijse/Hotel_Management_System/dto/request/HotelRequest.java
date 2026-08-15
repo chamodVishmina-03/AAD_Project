2cbdcf9dcafc06d17de203d531ea28e8392ab976
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:36:54 +0530

    add HotelRequest code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/request/HotelRequest.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/request/HotelRequest.java
@@ -1,4 +1,14 @@
 package com.ijse.Hotel_Management_System.dto.request;
 
-public class HotelRequest {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record HotelRequest(
+        @NotBlank String name,
+        String description,
+        @NotBlank String address,
+        @NotNull Long cityId,
+        Double starRating,
+        String phone,
+        String email
+) {}
