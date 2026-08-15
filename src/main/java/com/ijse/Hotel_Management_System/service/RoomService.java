b35e5a8ec119a4d23157a2e2a1e23a0c962d32b2
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:25:57 +0530

    update service file

--- src/main/java/com/ijse/Hotel_Management_System/service/RoomService.java
+++ src/main/java/com/ijse/Hotel_Management_System/service/RoomService.java
@@ -1,8 +1,8 @@
-package com.ijse.HOTEL_MANAGEMENT_SYSTEM.service;
+package com.ijse.Hotel_Management_System.service;
 
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.request.ImageRequest;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.request.RoomRequest;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.response.RoomResponse;
+import com.ijse.Hotel_Management_System.dto.request.ImageRequest;
+import com.ijse.Hotel_Management_System.dto.request.RoomRequest;
+import com.ijse.Hotel_Management_System.dto.response.RoomResponse;
 
 import java.time.LocalDate;
 import java.util.List;
