558348b758f1eeb7e6ecc5208e164545a4de1926
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 11:02:16 +0530

    add  ChatResponsedto code file

--- src/main/java/com/ijse/Hotel_Management_System/dto/response/ChatResponse.java
+++ src/main/java/com/ijse/Hotel_Management_System/dto/response/ChatResponse.java
@@ -1,4 +1,12 @@
 package com.ijse.Hotel_Management_System.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class ChatResponse {
+    private String reply;
 }
