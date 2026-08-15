c09d1376868a766ddf70121a7cf093fe9a7591d4
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:04:07 +0530

    create duplicateresourse  Exception

--- src/main/java/com/ijse/Hotel_Management_System/exception/BadRequestException.java
+++ src/main/java/com/ijse/Hotel_Management_System/exception/BadRequestException.java
@@ -1,4 +1,8 @@
 package com.ijse.Hotel_Management_System.exception;
 
-public class BadRequestException {
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
