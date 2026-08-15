fb5fb2bd32e593742a991a17c19b16abe287a2a1
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-15 17:25:06 +0530

    update service file

--- src/main/java/com/ijse/Hotel_Management_System/service/ReservationService.java
+++ src/main/java/com/ijse/Hotel_Management_System/service/ReservationService.java
@@ -1,8 +1,8 @@
-package com.ijse.HOTEL_MANAGEMENT_SYSTEM.service;
+package com.ijse.Hotel_Management_System.service;
 
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.request.BookingRequest;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.dto.response.BookingResponse;
-import com.ijse.HOTEL_MANAGEMENT_SYSTEM.enumeration.BookingStatus;
+import com.ijse.Hotel_Management_System.dto.request.BookingRequest;
+import com.ijse.Hotel_Management_System.dto.response.BookingResponse;
+import com.ijse.Hotel_Management_System.enumeration.BookingStatus;
 
 import java.util.List;
 
