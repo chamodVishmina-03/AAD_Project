eb7ebeba3984dd8c6f2860c35e305b227b4f5900
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-20 19:59:44 +0530

    create constant Response message

--- src/main/java/com/ijse/Hotel_Management_System/constant/ResponseMessage.java
+++ src/main/java/com/ijse/Hotel_Management_System/constant/ResponseMessage.java
@@ -1,5 +1,7 @@
 package com.ijse.Hotel_Management_System.constant;
 
 public class ResponseMessage {
+    public static String SUCCESS_MESSAGE = "Operation Successful";
+    public static String FAILURE_MESSAGE = "Operation Failed";
 
 }
