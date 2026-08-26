b6825494bedb4300c2e58a240b410f8287d4ecda
chamodvishmina <chamodvishmina4@gmail.com>
2026-08-26 22:17:34 +0530

    create admin icons js code

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1608,3 +1608,29 @@ async function loadAdminHotels() {
 }
 
 
+
+
+
+// admin icon
+
+const editIconSvg = `
+    <svg class="icon"
+         viewBox="0 0 24 24"
+         fill="none"
+         stroke="currentColor">
+        <path d="M17 3a2.85 2.85 0 114 4L7.5 20.5
+                 2 22l1.5-5.5L17 3z"/>
+    </svg>
+`;
+
+const trashIconSvg = `
+    <svg class="icon"
+         viewBox="0 0 24 24"
+         fill="none"
+         stroke="currentColor">
+        <path d="M3 6h18
+                 M8 6V4a2 2 0 012-2h4a2 2 0 012 2v2
+                 m3 0v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6h14z"/>
+    </svg>
+`;
+
