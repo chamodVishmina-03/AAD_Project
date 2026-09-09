93c61f4d9fbb3e905bf4d8ad6b2bf0cead931d79
chamodvishmina <chamodvishmina4@gmail.com>
2026-09-09 15:01:36 +0530

    update extra service

--- src/main/java/com/ijse/Hotel_Management_System/service/ExtraServiceService.java
+++ src/main/java/com/ijse/Hotel_Management_System/service/ExtraServiceService.java
@@ -1,14 +1,14 @@
 package com.ijse.Hotel_Management_System.service;
 
 import com.ijse.Hotel_Management_System.dto.request.ExtraServiceRequest;
-import com.ijse.Hotel_Management_System.entity.ExtraService;
+import com.ijse.Hotel_Management_System.dto.response.ExtraServiceResponse;
 
 import java.util.List;
 
 public interface ExtraServiceService {
-    ExtraService create(ExtraServiceRequest request);
-    ExtraService update(Long id, ExtraServiceRequest request);
-    List<ExtraService> findByHotel(Long hotelId);
-    ExtraService findById(Long id);
+    ExtraServiceResponse create(ExtraServiceRequest request);
+    ExtraServiceResponse update(Long id, ExtraServiceRequest request);
+    List<ExtraServiceResponse> findByHotel(Long hotelId);
+    ExtraServiceResponse findById(Long id);
     void delete(Long id);
 }
